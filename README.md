3ba041c8e843afd291eaed48add4c69f88daeb6b
Gabriel Mori <gabrielgimenezmori@gmail.com>
2025-02-15 16:39:39 -0300

    Adds text to speech

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ An AI character which you can talk to in real time.
 This is just a side project for now.
 
 # TODO
-- [ ] Text to speech
+- [x] Text to speech
 - [ ] Generate responses
 - [ ] Speech to text
 - [ ] Real time conversations (The AI decides the appropriate moment to answer)
---
+++ Services/text_to_speech.py
@@ -0,0 +1,30 @@
+import os
+import time
+from RealtimeTTS import TextToAudioStream, EdgeEngine
+
+class TextToSpeech:
+    def __init__(self, voice, language, pitch, rate):
+        print("[LOADING TEXT TO SPEECH ENGINE...]")
+        self.voice = voice
+        self.language = language
+        self.pitch = pitch
+        self.rate = rate
+        
+        self.engine = EdgeEngine(pitch=self.pitch, rate=self.rate)
+        self.engine.set_voice(self.voice)
+
+        print("[CREATING TEXT TO SPEECH AUDIO STREAM...]")
+        self.stream = TextToAudioStream(self.engine)
+
+    def play_stream(self):
+        if not self.stream.is_playing:
+            print("[PLAYING TEXT TO SPEECH...]")
+        self.stream.play(log_synthesized_text=True, language=self.language)
+
+    def say(self, text):
+        self.stream.feed(text)
+        self.play_stream()
+    
+    def say_stream(self, text_stream):
+        for chunk in text_stream: self.stream.feed(chunk.text)
+        self.play_stream()
